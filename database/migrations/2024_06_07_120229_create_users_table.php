ea6e591aeb4ed3238c029572469a8d23f7a7b815
Nkeneabasi Friday Aniekan <fridaynkeneabasi@gmail.com>
2024-06-27 13:32:58 +0100

    some changes

--- database/migrations/2024_06_07_120229_create_users_table.php
+++ database/migrations/2024_06_07_120229_create_users_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->boolean('is_verfied')->default(false);
+            $table->timestamp('email_verified_at')->default('')->nullable();
+            $table->boolean('is_verfied')->default('')->nullable();
             $table->string('password');
             $table->timestamps();
         });
