3b0a9f66ad9c3c94702cc5c8ae80478d70f48e24
Nkeneabasi Friday Aniekan <fridaynkeneabasi@gmail.com>
2024-06-28 15:00:56 +0100

    some update

--- config/logging.php
+++ config/logging.php
@@ -17,7 +17,21 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    // 'default' => env('LOG_CHANNEL', 'stack'),
+
+     [
+        'default' => env('LOG_CHANNEL', 'stack'),
+        'channels' => [
+            'stack' => [
+                'driver' => 'stack',
+                'channels' => ['single'],
+            ],
+            'single' => [
+                'driver' => 'errorlog',
+                'level' => 'debug',
+            ],
+        ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
