a2685a998aad5009e1cc9c9805a33c2ef6dea7c2
Nkeneabasi Friday Aniekan <fridaynkeneabasi@gmail.com>
2024-07-01 14:41:32 +0100

    update to user profile

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -95,7 +95,7 @@ class UserProfileController extends BaseController
 
     public function updateuserprofile(UserProfileRequest $request, $id)
     {   
-        dd($id);
+        dd($request->$id);
 
         $data = Validator::make($request->all(), $request->rules());
         
