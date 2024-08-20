a3292ec7527b7046604aae16219efa3e7b063b3e
Nkeneabasi Friday Aniekan <fridaynkeneabasi@gmail.com>
2024-08-20 14:39:35 +0100

    update to exam status endpoint for bult submission

--- app/Http/Controllers/ExamsStatusController.php
+++ app/Http/Controllers/ExamsStatusController.php
@@ -11,18 +11,32 @@ use Illuminate\Support\Facades\Validator;
 class ExamsStatusController extends Controller
 {
     public function ExamsResponse(Request $request){
-        $data = Validator::make($request->all(), [
-   
-            'question_id' => 'string|required',
-            'skipped' => 'boolean|required',
-            'answered' => 'boolean|required',
-            'option_chosen' => 'string|required',
-            'correct_option' => 'string|required' 
-        ]);
+
+        $data = $request->all();
+        
+        $validatedData = [];
+
+        foreach ($data as $key => $item) {
+            $validator = Validator::make($item(), [
+                'question_id' => 'string|required',
+                'skipped' => 'boolean|required',
+                'answered' => 'boolean|required',
+                'option_chosen' => 'string|required',
+                'correct_option' => 'string|required' 
+            ]);
+            
+            if($validator->fails()){
+                return response()->json([
+                    "error" => $validator->errors(),
+                    "failed_index" => $key
+                ],400);
+            }
+
+            $validatedData = $validator->validated();
+        }
 
 
-        $data = $request->post();
-        $createData = $this->create($data,);
+        $createData = $this->create($validatedData);
 
         return response([
             'data' => $createData,
@@ -31,17 +45,24 @@ class ExamsStatusController extends Controller
 
     public function create(array $data){
         $user = Auth::user();
+        
+        $createdData = [];
 
-        $examStatus = ExamStatus::create([
-            'question_id' => $data['question_id'],
-            'skipped' => $data['skipped'],
-            'answered' => $data['answered'],
-            'option_chosen' => $data['option_chosen'],
-            'correct_option' => $data['correct_option'],
+        foreach ($data as $item) {
+            $examStatus = ExamStatus::create([
+                'question_id' => $item['question_id'],
+                'skipped' => $item['skipped'],
+                'answered' => $item['answered'],
+                'option_chosen' => $item['option_chosen'],
+                'correct_option' => $item['correct_option'],
+            ]);
 
-        ]);
+            $user->ExamStatus()->save($examStatus);
+            $createdData = $examStatus;
+        }
 
-        $user->ExamStatus()->save($examStatus);
+        
+
 
         // $examStatus->user()->save();
     }
